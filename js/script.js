b5482d97db67fd5fabb86f5b38d8cebca583a172
Marcelo Nunes <marcelohenrique.nunes@gmail.com>
2022-05-04 15:29:58 -0300

    projeto completo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,84 @@
-let music = document.getElementById('music')
-let imagem = document.getElementById('imagem')
-let duracao = document.getElementById('progress')
+let musicas = [
+    {titulo:'Best Part Of Me', artista:'Ed Sheeran ft. Yebba', src:'./musicas/ed_sheeran_best_part_of_me_feat_yebba_official_lyric_video_3131468281568568928.mp3', img:'./imagens/best-part-img.jpg'},
+    {titulo:'Transmissão de Pensamento', artista:'Melim', src:'./musicas/melim_transmissao_de_pensamento_ao_vivo_-6431821278913897694.mp3', img:'./imagens/transmissao-melim.jpg'},
+    {titulo:'Off My Face', artista:'Justin Bieber', src:'./musicas/justin_bieber_off_my_face_live_from_paris_8094897589030745844.mp3', img:'./imagens/off-my-face.jpg'}
+]
 
-let tempoDuracaoFinal = parseInt(music.duration)
-let duracaoAtual = parseInt(music.currentTime)
+// variaveis
+let musica = document.querySelector('audio')
+let quantoDura = document.querySelector('.fim')
+let imagem = document.querySelector('img')
+let nomeMusica = document.querySelector('.informacoes h3')
+let nomeArtista = document.querySelector('.informacoes p')
+let indexMusica = 0
 
+mudarDeMusica(indexMusica)
 
+quantoDura.textContent = minutos(Math.floor(musica.duration))
+
+// eventos
+musica.addEventListener('timeupdate', atualizando)
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--
+    mudarDeMusica(indexMusica)
+    if (indexMusica < 0) {
+        indexMusica = 2
+    }
+    musica.play()
+    iniciar()
+})
+document.querySelector('.posterior').addEventListener('click', () => {
+    indexMusica++
+    mudarDeMusica(indexMusica)
+    if (indexMusica > 2) {
+        indexMusica = 0
+    }
+    musica.play()
+    iniciar()
+
+})
+
+// funcoes
 function iniciar() {
-    music.play()
-    console.log(music.currentTime)
+       
+    musica.play()
+    document.querySelector('.botao-pause').style.display = 'block'
+    document.querySelector('.botao-play').style.display = 'none'
+
 }
 
 function pausar() {
-    music.pause()
+       
+    musica.pause()
+    document.querySelector('.botao-pause').style.display = 'none'
+    document.querySelector('.botao-play').style.display = 'block'
+
 }
+
+function atualizando() {
+    let barra = document.querySelector('progress')
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
+    let tempo = document.querySelector('.inicio')
+    tempo.textContent = minutos(Math.floor(musica.currentTime))
+}
+
+function minutos(segundos) {
+    let segundosParaMinutos = Math.floor(segundos / 60)
+    let minutosParaSegundos = segundos % 60
+    if (minutosParaSegundos < 10) {
+        minutosParaSegundos = '0' + minutosParaSegundos
+    } 
+
+    return segundosParaMinutos + ':' + minutosParaSegundos
+}
+
+function mudarDeMusica(index) {
+    musica.setAttribute('src', musicas[index].src)
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo
+        nomeArtista.textContent = musicas[index].artista
+        imagem.src = musicas[index].img
+        quantoDura.textContent = minutos(Math.floor(musica.duration))
+    })
+}
+
